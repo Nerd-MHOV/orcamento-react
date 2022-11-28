8ddd5f535c80402c3f5ea973a25db8ae661c8a59
Matheus Henrique <nerd@pop-os.localdomain>
2022-11-28 20:15:45 -0300

    refing

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -12,6 +12,68 @@ async function main() {
       phone: "14 991578451",
     },
   });
+
+  const food = await prismaClient.foods.upsert({
+    where: {
+      id: 1,
+    },
+    update: {},
+    create: {
+      id: 1,
+      adt: 87,
+      adtex: 87,
+      chd0: 0,
+      chd4: 43,
+      chd8: 52,
+    },
+  });
+
+  const categoriesPAD = await prismaClient.categories.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      initials: "PAD",
+      name: "padrão",
+    },
+  });
+
+  const categoriesPADV = await prismaClient.categories.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      initials: "PADV",
+      name: "padrão varanda",
+    },
+  });
+
+  const categoriesLUX = await prismaClient.categories.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      initials: "LUX",
+      name: "luxo",
+    },
+  });
+
+  const categoriesLUXC = await prismaClient.categories.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      id: 1,
+      initials: "LUXC",
+      name: "luxo conjugado",
+    },
+  });
+
+  const categoriesLUXH = await prismaClient.categories.upsert({
+    where: { id: 5 },
+    update: {},
+    create: {
+      id: 1,
+      initials: "LUXH",
+      name: "luxo com hidro",
+    },
+  });
 }
 
 main()
@@ -19,7 +81,7 @@ main()
     await prismaClient.$disconnect();
   })
   .catch(async (e) => {
-    console.log(e);
+    console.error(e);
     await prismaClient.$disconnect();
     process.exit(1);
   });
