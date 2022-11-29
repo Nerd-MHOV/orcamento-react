98c6423e656583cf0a8cf0443a2a458bfc1a9021
Nerd-MHOV <matheus.henrique4245@gmail.com>
2022-11-28 23:14:54 -0300

    tratamento de erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,29 @@
-import express from 'express'
-import routes from './routes'
-import cors from 'cors'
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error";
 
-require('dotenv').config()
-
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT_SERVER || 3333
+const port = process.env.PORT_SERVER || 3333;
 
-//cors 
-const whiteList = ["http://localhost:5173"]
-app.use(cors({
-    origin: ( origin, callback ) => {
-        if (!origin || whiteList.indexOf(origin) !== -1) 
-            callback(null, true)
-        else 
-            callback(new Error("Not allowed by CORS"))
+//cors
+const whiteList = ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || whiteList.indexOf(origin) !== -1) callback(null, true);
+      else callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-}))
+  })
+);
 
+app.use(express.json());
+app.use(routes);
 
-app.use(express.json())
-app.use(routes)
-
-try {
-    app.listen(3333, () => {
-        console.log('Server is running in ' + port)
-    })
-} catch (err) {
-    console.log(err)
-}
+app.use(errorMiddleware);
+app.listen(3333, () => {
+  console.log("Server is running in " + port);
+});
