57e71b21dc072679f79c32ad0f2ade541eb697dc
Matheus <matheus.henrique4245@gmail.com>
2024-04-10 12:32:51 -0300

    fix erro if not a hava a contact in deal

--- server/src/services/chatguru/EditField.ts
+++ server/src/services/chatguru/EditField.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import config from "../../config/chatGuruConfig"
 import {addPrefixToObjectKeys} from "./AddPrefixToObj";
+import { resolve } from "path";
 
 export async function EditField(
     number: string,
@@ -14,16 +15,21 @@ export async function EditField(
 ) {
     // &field__email=email;
     const objField = addPrefixToObjectKeys(fields, "field__")
-    return (await axios.post(
-        base_url, {}, {
-            params: {
-                action: "chat_update_custom_fields",
-                key,
-                account_id,
-                phone_id,
-                chat_number: number,
-                ...objField,
-            }
-        })).data;
+    return new Promise((resolve, rejects) => {
+         axios.post(
+            base_url, {}, {
+                params: {
+                    action: "chat_update_custom_fields",
+                    key,
+                    account_id,
+                    phone_id,
+                    chat_number: number,
+                    ...objField,
+                }
+            }).then((response) => {
+                resolve(response.data)
+            }).catch(error => {
+                rejects(error)
+            })
+    });
 }
-
